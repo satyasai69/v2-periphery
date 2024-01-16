fc3c5c077fc9da53f90b932dee5544df831a79b7
satyasai69 <ch.satya.sai133@gmail.com>
2024-01-16 22:03:02 +0530

    hi

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'837dc96bf2691f4a8b9e7583cd55370f86bd07b2f1c6490217086968f4b5bb5e' // init code hash
             ))));
     }
 
